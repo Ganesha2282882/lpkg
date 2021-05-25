e3c1053f3a831acabb5c5de9e757591276bde664
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-05-25 12:13:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "+ Getting script"
 wget -O /usr/local/bin/lpkg https://raw.githubusercontent.com/Ganesha2282882/lpkg/main/lpkg.sh -q --show-progress
 chmod +x /usr/local/bin/lpkg
 echo "+ Installing dependencies"
-apt-get install git -y || /bin/bash -c 'yes | pacman -S git' || dnf in git -y
+apt-get install git subversion -y || /bin/bash -c 'yes | pacman -S git subversion' || dnf in git subversion -y
