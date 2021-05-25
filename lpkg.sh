f180720dc5caea1d5dac6aa71a708f3b1380606e
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-05-25 12:30:54 -0400

    Update lpkg.sh

--- lpkg.sh
+++ lpkg.sh
@@ -1,1 +1,31 @@
-
+#!/bin/bash
+cd /var/tmp
+case $1 in
+'-E')
+svn checkout https://github.com/Ganesha2282882/lpkg.git/trunk/packages/$2 lpkg_pkg_src
+cd lpkg_pkg_src
+source PKGSCRIPT
+cd ..
+git clone $git_url lpkg_pkg_bld
+cd lpkg_pkg_bld
+build
+;;
+'-F')
+svn checkout https://github.com/Ganesha2282882/lpkg.git/trunk/packages/$2 lpkg_pkg_fnd &&\
+(echo Package is available. ; rm -rf lpkg_pkg_fnd) ||\
+echo Package does not exist.
+;;
+'-P')
+svn checkout https://github.com/Ganesha2282882/lpkg.git/trunk/packages/$2 lpkg_pkg_chk
+less lpkg_pkg_chk/PKGSCRIPT
+;;
+'-D')
+svn checkout https://github.com/Ganesha2282882/lpkg.git/trunk/packages/$2 lpkg_pkg_dnl
+cd lpkg_pkg_dnl
+source PKGSCRIPT
+cd ..
+git clone $git_url lpkg_pkg_dl2
+mv lpkg_pkg_dl2 "/tmp/PKG_SOURCE"
+echo "Source is available at /tmp/PKG_SOURCE."
+;;
+esac
